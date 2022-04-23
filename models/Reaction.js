a162b5432d673b8f001a0fb8dadd45313a472443
bgould86 <bgould86@gmail.com>
2022-04-23 13:38:46 -0700

    getting caught up to thursday

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("./../utils/dateFormat");
+
+// Schema to create a course model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("./../utils/dateFormat");
+const reactionSchema = require("./Reaction");
+
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 200,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
